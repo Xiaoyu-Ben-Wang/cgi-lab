239d3c3f8de50dea87943ba523da1356795202a8
Ben Wang <xiaoyu.w.99@gmail.com>
2021-09-23 21:24:22 -0600

    create hello.py according to lab instructions

---
+++ hello.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+import os
+import json
+import templates
+import cgi
+import secret
+
+
+def main():
+
+    # Question 1
+    '''
+    for k in os.environ.keys():
+        print(f"{k}: {os.environ[k]}")
+    '''
+
+    '''
+    environment_json = json.dumps(dict(os.environ), indent=4)
+    print("<h3>JSON Environment</h3>")
+    print(f"<p>{environment_json}</p>")
+    '''
+
+    # Question 2
+    '''
+    queries = os.environ["QUERY_STRING"].split('&')
+    print("<h3>Query Parameters</h3>")
+    for q in queries:
+        print(f"<p>{q}</p>")
+    '''
+    # Question 3
+    '''
+    print("<h3>Browser</h3>")
+    print(f"<p>{os.environ['HTTP_USER_AGENT']}</p>")
+    '''
+    # Question 4, 5, 6
+    form = cgi.FieldStorage()
+    username = form.getvalue("username")
+    password = form.getvalue("password")
+    if not (username and password):
+        print(templates.login_page())
+    else:
+
+        login_immediate = False
+        if username==secret.username and password==secret.password:
+            print("Set-Cookie:LoggedIn=true;", end="")
+            print("Set-Cookie:TestCookie=true;\r\n")
+            login_immediate = True
+
+
+
+        cookies = [c.strip() for c in os.environ.get("HTTP_COOKIE").split(';')]
+
+        login_cookie = "LoggedIn=true" in cookies
+        if login_cookie or login_immediate:
+            print(templates.secret_page(username, password))
+        else:
+            print(templates.after_login_incorrect())
+
+
+
+
+
+if __name__ == '__main__':
+    main()
